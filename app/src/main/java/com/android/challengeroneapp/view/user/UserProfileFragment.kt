d10144904529e5b391b33649710147dd2e16b00a
Anjum <252516@NTTDATA.COM>
2022-04-22 14:36:48 -0500

    Added databiding to user Profile view

--- app/src/main/java/com/android/challengeroneapp/view/user/UserProfileFragment.kt
+++ app/src/main/java/com/android/challengeroneapp/view/user/UserProfileFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.android.challengeroneapp.R
 import com.android.challengeroneapp.data.db.entity.ProfileEntity
+import com.android.challengeroneapp.databinding.FragmentUserProfileBinding
 import com.android.challengeroneapp.viewmodel.ProfileDataViewModel
 
 class UserProfileFragment : Fragment() {
@@ -19,21 +21,15 @@ class UserProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val binding: FragmentUserProfileBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_user_profile, container, false)
 
         viewModel =  ViewModelProvider(this).get(ProfileDataViewModel::class.java)
         viewModel.getProfileData().observe(viewLifecycleOwner, Observer {
             it?.let {
-                updateUserData(it[0])
+                binding.profile = it[0]
             }
         })
-    }
-
-    private fun updateUserData(profile: ProfileEntity) {
-        // to-do
+        return binding.root
     }
 }
